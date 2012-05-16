cee9bfee73382f5a495de775425529e1a949d5a1
Sergey Hanchar <hanchar.sergey@gmail.com>
2012-05-16 13:37:37 +0300

    Save location poits to file. Send file data to server.

--- FindMeGPS/ViewController.m
+++ FindMeGPS/ViewController.m
@@ -22,30 +22,32 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  
-  locationManager = [[CLLocationManager alloc] init];
-  
-  // Best performance
-  locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-  
-  // Distance filter
-  locationManager.distanceFilter = 100;
-  
-  locationManager.delegate = self;
-  [locationManager startUpdatingLocation];
-  
+    
+  [self initializeLocationManager];  
 }
 
 - (void)viewDidUnload {
+  
   [self setCurrentLocationLat:nil];
   [self setCurrentLocationLng:nil];
   [self setOldLocationLat:nil];
   [self setOldLocationLng:nil];
+  
   [super viewDidUnload];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
   return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (void)initializeLocationManager {
+  locationManager = [[CLLocationManager alloc] init];
+  
+  locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+  locationManager.distanceFilter = 100.0f;
+  locationManager.delegate = self;
+  
+  [locationManager startUpdatingLocation];
 }
 
 - (void)locationManager:(CLLocationManager *)manager
@@ -58,22 +60,56 @@
   self.oldLocationLat.text = [NSString stringWithFormat:@"%+.20f", oldLocation.coordinate.latitude];
   self.oldLocationLng.text = [NSString stringWithFormat:@"%+.20f", oldLocation.coordinate.longitude];
   
-  [self postLocationCoordinates:newLocation.coordinate.latitude :newLocation.coordinate.longitude];
+  [self writeLocationsToFile:newLocation];
+  [self sendLocationsJsonToServer];
 }
 
-- (void) postLocationCoordinates:(float)latitude:(float)longitude {
-  NSString *post = [NSString stringWithFormat:@"latitude=%+.20f&longitude=%+.20f", latitude, longitude];
-  NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-  NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+- (void) writeLocationsToFile:(CLLocation *)newLocation {
+  NSFileManager *fileManager = [NSFileManager defaultManager];
+  NSString *locationsFileHomePath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Locations"];
   
-  NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-  [request setURL:[NSURL URLWithString:@"http://sergey-hanchar:3000/"]];
+  NSMutableString *locationsFileData = [NSMutableString stringWithString:@""];
+  if ([fileManager fileExistsAtPath:locationsFileHomePath]) {
+    locationsFileData = [NSMutableString stringWithContentsOfFile:locationsFileHomePath encoding:NSUTF8StringEncoding error:nil];
+  }
+  
+  NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+  [dateFormat setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+  
+  [locationsFileData appendString:[NSString stringWithFormat:@"latitude/%+.20f/longitude/%+.20f/horizontal_accuracy/%.2f/speed/%+.2f/created_at/%@;", newLocation.coordinate.latitude, newLocation.coordinate.longitude, newLocation.horizontalAccuracy, newLocation.speed, [dateFormat stringFromDate:newLocation.timestamp]]];
+
+  [locationsFileData writeToFile:locationsFileHomePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+}
+
+- (void)sendLocationsJsonToServer {
+  NSString *filePath = [NSHomeDirectory() stringByAppendingPathComponent:@"/Documents/Locations"];
+  NSString *fileData = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+  
+  NSDictionary *fileJson = [NSDictionary dictionaryWithObjectsAndKeys:fileData, @"location", nil];
+  NSData *jsonData = [NSJSONSerialization dataWithJSONObject:fileJson options:NSJSONWritingPrettyPrinted error:nil];
+  
+  NSURL *url = [NSURL URLWithString:@"http://sergey-hanchar:3000/api/devices/1/locations/"];
+  NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
   [request setHTTPMethod:@"POST"];
-  [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-  [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-  [request setHTTPBody:postData];
+  [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+  [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+  [request setValue:[NSString stringWithFormat:@"%d", [jsonData length]] forHTTPHeaderField:@"Content-Length"];
+  [request setHTTPBody:jsonData];
   
-  // Coonecting to server
-  [[NSURLConnection alloc] initWithRequest:request delegate:self];
+  NSHTTPURLResponse *responseCode = nil;
+  NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&responseCode error:nil];
+  
+  if ([responseCode statusCode] == 200) {
+    NSDictionary *responseJson = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:nil];
+    
+    if ([[responseJson valueForKey:@"saved"] boolValue] == YES) {
+      NSFileManager *fileManager = [NSFileManager defaultManager];
+      NSString *locationPointsHomePath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Locations"];
+      
+      if ([fileManager fileExistsAtPath:locationPointsHomePath]) {
+        [fileManager removeItemAtPath:locationPointsHomePath error:nil];
+      }
+    }
+  }
 }
 @end
